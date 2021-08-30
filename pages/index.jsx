66214b7d1387f89b0cec742b65c0d0951e6fac88
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-30 10:59:42 +0300

    removed state and useeffect, getstaticprops fn

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import MeetupList from './../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -18,23 +17,31 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  const [meetupsArr, setMeetupsArr] = useState([]);
-
-  useEffect(() => {
-    setMeetupsArr(DUMMY_MEETUPS);
-  }, []);
-
+const HomePage = (props) => {
   return (
     <>
       <h1>Home meetup page</h1>
-      <MeetupList meetups={meetupsArr} />
+      <MeetupList meetups={props.meetups} />
     </>
   );
 };
 
 // tam kad puslapis butu sugeneruotas duomenims pasikeitus yra naudojami 2 budai
 // SSR server side rendering - duomenys sugeneruojami uzklausos metu, labiau tinka kai duomenys kinta kas sekunde ar greiciau
-// SSG static site generating - duomenys sugeneruojami aplikacijos sukurimo metu ir atnaujinami jei reikia tam tikru intervalu
+// SSG static site generating - duomenys sugeneruojami aplikacijos sukurimo metu ir atnaujinami jei reikia tam tikru intervalu, greitesnis uz SSR
+
+export function getStaticProps() {
+  // sitas kodas niekada neatsidurs pas klienta
+  // veikia tiktai pages, pavadinimas butinai toks pats
+  // cia galima sakyti yra back end erdve
+  // galetu buti fetch, validacija ir pan
+  // paduodam duomenis ir nebereikia state
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10, // kas 10s duomenys atnaujinami
+  };
+}
 
 export default HomePage;
