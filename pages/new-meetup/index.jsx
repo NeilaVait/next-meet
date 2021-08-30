2686c57167c523d34e9fd6033b3b25afccaf2c30
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-30 14:12:30 +0300

    redirect after adding meetup

--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import NewMeetupForm from './../../components/meetups/NewMeetupForm';
+import { useRouter } from 'next/router';
 
 const NewMeetup = () => {
+  const router = useRouter();
   async function addMeetupHandler(enteredMeetupData) {
     console.log(enteredMeetupData);
     // send data to api
     const result = await axios.post('/api/new-meetup', enteredMeetupData);
     console.log(result.data);
+    router.push('/');
   }
 
   return (
