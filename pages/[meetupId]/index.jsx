26e3c9e7f2f3bccde3901307153013ad9a4c90a3
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-08-30 11:42:23 +0300

    getStaticProps data

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -1,17 +1,52 @@
 import MeetupDetail from './../../components/meetups/MeetupDetail';
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <>
       <MeetupDetail
-        id="m1"
-        title="The first meetup"
-        image="https://picsum.photos/id/1067/900/600/"
-        address="Some street 5, 23548, New York, New York"
-        description="First meetup in NY"
+        title={props.meetupData.title}
+        image={props.meetupData.image}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
       />
     </>
   );
 };
+
+export function getStaticPaths() {
+  // nurodo pagal kokia dinamine informacija sugeneruoti statinius puslapius
+  return {
+    fallback: false, // TRUE jei einam i psl kurio nera aprasyta paths tai tas puslapis sugeneruojamas uzklausos metu(at run time)
+    // FALSE gaunam 404 jei puslapio nera
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export function getStaticProps(context) {
+  console.log(context.params.meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: 'm1',
+        title: 'The first meetup',
+        image: 'https://picsum.photos/id/1067/900/600/',
+        address: 'Some street 5, 23548, New York, New York',
+        description: 'First meetup in NY',
+      },
+    },
+    revalidate: 5,
+  };
+}
 
 export default MeetupDetails;
